73a6989a995296269056d341cab2e0da294a6025
KiarieLinus <kiarielinus@gmail.com>
2022-11-09 18:05:55 +0300

    Added Required Fonts

--- app/src/main/java/com/kiarielinus/countries/ui/theme/Type.kt
+++ app/src/main/java/com/kiarielinus/countries/ui/theme/Type.kt
@@ -2,9 +2,22 @@ package com.kiarielinus.countries.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kiarielinus.countries.R
+
+val ElsieSwashCaps = FontFamily(
+    Font(R.font.elsie_swash_caps_black)
+)
+
+val Axiforma = FontFamily(
+    Font(R.font.kastelov_axiforma_bold, FontWeight.W700),
+    Font(R.font.kastelov_axiforma_regular, FontWeight.W400),
+    Font(R.font.kastelov_axiforma_medium, FontWeight.W500),
+    Font(R.font.kastelov_axiforma_light, FontWeight.W300)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
